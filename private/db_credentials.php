f7dca3efb7056a056f86754daaa675b122eda89f
Jaimal Dullat <jaimaldullat@gmail.com>
2019-03-13 01:42:10 -0400

    update db credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,7 +1,16 @@
 <?php
 // Define constants for database connectivity
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'globe_bank');
+//define('DB_SERVER', 'localhost');
+//define('DB_USER', 'root');
+//define('DB_PASS', '');
+//define('DB_NAME', 'globe_bank');
+
+
+// Define constants for database connectivity
+define('DB_SERVER', 'us-cdbr-iron-east-03.cleardb.net');
+define('DB_USER', 'b727689a3764be');
+define('DB_PASS', 'a9804dea');
+define('DB_NAME', 'heroku_eb7071a5fc4c02f');
+
+
 ?>
